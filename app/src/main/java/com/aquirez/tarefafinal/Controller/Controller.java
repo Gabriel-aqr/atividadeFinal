a4d8cc7d5cc5db992eca78bd0059cd0144102de6
Gabriel-aqr <gabrielaqr2000@gmail.com>
2025-07-07 16:51:41 -0300

    melhorado

--- app/src/main/java/com/aquirez/tarefafinal/Controller/Controller.java
+++ app/src/main/java/com/aquirez/tarefafinal/Controller/Controller.java
@@ -7,26 +7,26 @@ import android.content.ContentValues;
 import com.aquirez.tarefafinal.database.database;
 
 public class Controller {
-    private database helper;
+    private database DBhelper;
 
     public Controller(Context context) {
-        helper = new database(context);
+        DBhelper = new database(context);
     }
 
     public boolean cadastrar(String u, String s) {
-        SQLiteDatabase db = helper.getWritableDatabase();
-        ContentValues cv = new ContentValues();
-        cv.put("usuario", u);
-        cv.put("senha", s);
-        long id = db.insert("users", null, cv);
+        SQLiteDatabase db = DBhelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("usuario", u);
+        contentValues.put("senha", s);
+        long id = db.insert("users", null, contentValues);
         return id > 0;
     }
 
     public boolean login(String u, String s) {
-        SQLiteDatabase db = helper.getReadableDatabase();
-        Cursor c = db.query("users", null, "usuario=? AND senha=?", new String[]{u,s}, null,null,null);
-        boolean ok = c.moveToFirst();
-        c.close();
+        SQLiteDatabase db = DBhelper.getReadableDatabase();
+        Cursor cursor = db.query("users", null, "usuario=? AND senha=?", new String[]{u,s}, null,null,null);
+        boolean ok = cursor.moveToFirst();
+        cursor.close();
         return ok;
     }
 }
--- app/src/main/java/com/aquirez/tarefafinal/MainActivity.java
+++ app/src/main/java/com/aquirez/tarefafinal/MainActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import com.aquirez.tarefafinal.Controller.Controller;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,11 +18,19 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        usuario = findViewById(R.id.userId);
-        senha = findViewById(R.id.senhaId);
-        controller = new Controller(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            usuario = findViewById(R.id.userId);
+            senha = findViewById(R.id.senhaId);
+            controller = new Controller(this);
+
+            return insets;
+        });
     }
 
     public void btnEntrar(View view) {
